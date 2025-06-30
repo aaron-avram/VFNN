ad5f807349c917e346c59520215ec9dfe802c6b8
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2025-06-30 18:44:56 -0400

    normalizing done

--- src/net/parse/transform_data.py
+++ src/net/parse/transform_data.py
@@ -5,10 +5,8 @@ import torch
 from net.parse.csv_parser import merge_on_date, parse_path
 from net.parse.finance_parser import get_data
 
-def to_tensor(trends: pd.DataFrame, stats: pd.DataFrame, delta: int=3) -> torch.Tensor:
-    merged = pd.merge(trends, stats, how='outer', on='date')
-    data = downsample(merged, delta)
-    return data
+def merge(trends: pd.DataFrame, stats: pd.DataFrame) -> torch.Tensor:
+    return pd.merge(trends, stats, how='outer', on='date')
 
 def downsample(df: pd.DataFrame, start: int = 1, delta: int=3) -> torch.Tensor:
     new_size = (len(df) - start) // delta
@@ -19,17 +17,35 @@ def downsample(df: pd.DataFrame, start: int = 1, delta: int=3) -> torch.Tensor:
         col = df[col_name]
         for i in range(start, len(df) // delta - 1, delta):
             if j == 26:
-                out[i, j] = np.sqrt(sum(col[k] for k in range(i, i + delta)))
+                out[i, j] = np.sqrt(sum(col[k] ** 2 for k in range(i, i + delta)))
             elif j == 27:
                 out[i, j] = sum(col[k] for k in range(i, i + delta))
             else:
                 out[i, j] = sum(col[k] for k in range(i, i + delta)) / delta
     return out
 
+def normalize(tensor: torch.Tensor, window: int | float = float('inf')):
+    if window == float('inf'):
+        mean = tensor.mean(dim=1, keepdim=True)
+        std = tensor.std(dim=1, keepdim=True) + 1e-12
+        new = (tensor - mean) / std
+    else:
+        new = torch.zeros_like(tensor)
+        for i in range(0, tensor.shape[0], window):
+            end = i + window
+            cur = tensor[i:end, :] # Alias for convenience
+            mean = cur.mean(dim=1, keepdim=True)
+            std = cur.std(dim=1, keepdim=True) + 1e-12
+
+            new[i:end, :] = (cur - mean) / std
+    return new
+
+def rolling_window(tens: torch.Tensor, context: int = 8):
+    return
 if __name__ == '__main__':
     path = Path('../../../data/trends')
     path_data = parse_path(path)
     _trends = merge_on_date(path_data)
     _stats = get_data()
-    _data = to_tensor(_trends, _stats)
-
+    _data = merge(_trends, _stats)
+    tens = downsample(_data)

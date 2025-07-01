f07de533260f226003b9d01d84fa08f24c973817
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2025-06-30 20:51:30 -0400

    Full data processing pipeline finished

--- src/net/parse/transform_data.py
+++ src/net/parse/transform_data.py
@@ -5,43 +5,65 @@ import torch
 from net.parse.csv_parser import merge_on_date, parse_path
 from net.parse.finance_parser import get_data
 
-def merge(trends: pd.DataFrame, stats: pd.DataFrame) -> torch.Tensor:
+def merge(trends: pd.DataFrame, stats: pd.DataFrame) -> pd.DataFrame:
     return pd.merge(trends, stats, how='outer', on='date')
 
 def downsample(df: pd.DataFrame, start: int = 1, delta: int=3) -> torch.Tensor:
     new_size = (len(df) - start) // delta
     features = df.shape[1] - 1
     out = torch.zeros(new_size, features)
-    numerics = df.select_dtypes(include='number').columns
-    for j, col_name in enumerate(numerics):
-        col = df[col_name]
-        for i in range(start, len(df) // delta - 1, delta):
-            if j == 26:
-                out[i, j] = np.sqrt(sum(col[k] ** 2 for k in range(i, i + delta)))
-            elif j == 27:
-                out[i, j] = sum(col[k] for k in range(i, i + delta))
+    numerics = df.select_dtypes(include='number').values
+    VIX_COL = df.columns.get_loc('volatility')
+    RET_COL = df.columns.get_loc('returns')
+    for i in range(new_size):
+        chunk = numerics[start + delta * i: start + delta * (i + 1)]
+        for j in range(features):
+            if j == VIX_COL:
+                out[i, j] = np.sqrt(chunk[:, j])
+            elif j == RET_COL:
+                out[i, j] = np.sum(chunk[:, j])
             else:
-                out[i, j] = sum(col[k] for k in range(i, i + delta)) / delta
+                out[i, j] = np.mean(chunk[:, j])
     return out
 
-def normalize(tensor: torch.Tensor, window: int | float = float('inf')):
-    if window == float('inf'):
-        mean = tensor.mean(dim=1, keepdim=True)
-        std = tensor.std(dim=1, keepdim=True) + 1e-12
+def normalize(tensor: torch.Tensor, k: int | float = float('inf')):
+    if k == float('inf'):
+        mean = tensor.mean(dim=0, keepdim=True)
+        std = tensor.std(dim=0, keepdim=True) + 1e-12
         new = (tensor - mean) / std
     else:
         new = torch.zeros_like(tensor)
-        for i in range(0, tensor.shape[0], window):
-            end = i + window
+        for i in range(0, tensor.shape[0], k):
+            end = min(i + k, tensor.shape[0])
             cur = tensor[i:end, :] # Alias for convenience
-            mean = cur.mean(dim=1, keepdim=True)
-            std = cur.std(dim=1, keepdim=True) + 1e-12
+            mean = cur.mean(dim=0, keepdim=True)
+            std = cur.std(dim=0, keepdim=True) + 1e-12
 
             new[i:end, :] = (cur - mean) / std
     return new
 
-def rolling_window(tens: torch.Tensor, context: int = 8):
-    return
+def rolling_window(tensor: torch.Tensor, context: int = 10) -> tuple[torch.Tensor]:
+    data = tensor.unfold(dimension=0, size=context)
+    targets = tensor[context:, -2] # Get volatility (a bit clumsy)
+
+    return data, targets
+
+def build_data(trends: pd.DataFrame, stats: pd.DataFrame, delta: int = 3, k: int = float('inf'), context: int = 10) -> tuple[torch.Tensor]:
+    downsampled = downsample(merge(trends, stats), delta)
+
+    # Split
+    b1 = int(downsampled.shape[0] * 0.8)
+    b2 = int(downsampled.shape[0] * 0.9)
+    splits = (downsampled[:b1], downsampled[b1:b2], downsampled[b2:])
+    normed = list(map(lambda x: normalize(x, k), splits))
+
+    # build splits
+    x_train, y_train = rolling_window(normed[0], context)
+    x_test, y_test = rolling_window(normed[1], context)
+    x_dev, y_dev = rolling_window(normed[2], context)
+
+    return x_train, y_train, x_test, y_test, x_dev, y_dev
+
 if __name__ == '__main__':
     path = Path('../../../data/trends')
     path_data = parse_path(path)

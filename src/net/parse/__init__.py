691587babf6d8ba74cb88a3ca8ad546100333360
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2025-07-04 16:06:22 -0400

    Done

--- src/net/parse/__init__.py
+++ src/net/parse/__init__.py
@@ -2,7 +2,7 @@
 Init file for the parse module
 """
 from .csv_parser import parse_path, merge_on_date, Path
-from .finance_parser import get_data
+from .finance_parser import get_data, get_data_garch
 from .transform_data import build_data
 
-__all__ = ['parse_path', 'merge_on_date', 'get_data', 'build_data', 'Path']
+__all__ = ['parse_path', 'merge_on_date', 'get_data', 'build_data', 'Path', 'get_data_garch']
--- src/net/parse/finance_parser.py
+++ src/net/parse/finance_parser.py
@@ -3,14 +3,14 @@ import pandas as pd
 import numpy as np
 from net.parse.csv_parser import interpolate_df
 
-def get_data(start: str = '2004-10-19', end: str = '2015-07-24'):
+def get_data(start: str = '2004-10-19', end: str = '2015-07-24', scale=1e6):
     daily = yf.download(tickers='^SPX', start=start, end=end, interval='1d')
     close = np.array(daily[['Close']]).flatten()
     opn = np.array(daily[['Open']]).flatten()
     high = np.array(daily[['High']]).flatten()
     low = np.array(daily[['Low']]).flatten()
 
-    vol, retr = _calc_stats(close, high, low, opn)
+    vol, retr = _calc_stats(close, high, low, opn, scale)
 
 
     df = pd.DataFrame({
@@ -27,11 +27,11 @@ def _calc_stats(close: pd.DataFrame, high: pd.DataFrame, low: pd.DataFrame, opn:
     d = np.log(np.divide(low, opn))
     c = np.log(np.divide(close, opn))
 
-    vol = (0.511 * ((u - d)**2) - 0.019 * (c * (u + d) - 2 * u * d) - 0.383 * (c ** 2)) * scale
-    retr = np.log(close[1:] / close[:-1]) * scale
+    vol = (0.511 * ((u - d)**2) - 0.019 * (c * (u + d) - 2 * u * d) - 0.383 * (c ** 2))
+    retr = np.log(close[1:] / close[:-1])
     retr = np.insert(retr, 0, 0) # For shape alignment
 
     return vol, retr
 
 if __name__ == '__main__':
-    print(get_data())
+    print(get_data_garch())
